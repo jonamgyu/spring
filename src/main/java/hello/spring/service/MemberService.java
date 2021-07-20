4db80f087dac3101ac6e39802a56fb01317b334f
namgyu <darkaxe@gmail.com>
2021-07-20 23:30:23 +0900

    Spring Study

--- src/main/java/hello/spring/service/MemberService.java
+++ src/main/java/hello/spring/service/MemberService.java
@@ -2,6 +2,9 @@ package hello.spring.service;
 
 import hello.spring.domain.Member;
 import hello.spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +13,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
